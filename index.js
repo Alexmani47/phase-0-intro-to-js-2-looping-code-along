26c57d929377b026fe402450f379acbc29aeefa9
Alex <alex.mani@student.moringaschool.com>
2025-03-10 09:32:32 +0300

    Complete

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-function writeCards(names, event) {
-    let messages = [];
-    for (let i = 0; i < names.length; i++) {
-        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
-    }
-    return messages;
+function writeCards(names, occasion) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+  }
+  return messages;
+}
+function countDown(num) {
+  while (num >= 0) { 
+      console.log(num);
+      num--;
+  }
 }
 
-function countDown(number) {
-    while (number >= 0) {
-        console.log(number);
-        number--;
-    }
-}
+
